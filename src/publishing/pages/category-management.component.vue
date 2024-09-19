00be275071406204f16af8292cb8dfc3d5d50594
Angel Velasquez <pcisavel@upc.edu.pe>
2024-09-19 16:48:51 -0500

    feat(categories): add category management component.

---
+++ src/publishing/pages/category-management.component.vue
@@ -0,0 +1,28 @@
+<script>
+import {Category} from "../model/category.entity.js";
+
+export default {
+  name: "category-management",
+  data() {
+    return {
+      title: { singular: "Category", plural: "Categories" },
+      categories: [],
+      category: new Category({}),
+      selectedCategories: [],
+      categoryService: null,
+      createAndEditDialogIsVisible: false,
+      isEdit: false,
+      submitted: false
+    }
+  }
+
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>

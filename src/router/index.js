0bc463b7c00f809d06b3fed4538e2506edcee6be
Angel Velasquez <pcisavel@upc.edu.pe>
2024-09-26 15:46:01 -0500

    feat(in-app-navigation): add categories navigation rule.

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,15 @@
 import {createRouter, createWebHistory} from "vue-router";
 import HomeComponent from "../public/pages/home.component.vue";
 import AboutComponent from "../public/pages/about.component.vue";
+import CategoryManagementComponent from "../publishing/pages/category-management.component.vue";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/home', name: 'home', component: HomeComponent, meta: { title: 'Home'} },
-        { path: '/about', name: 'about', component: AboutComponent, meta: { title: 'About us'} },
-        { path: '/', redirect: '/home' }
+        { path: '/home',                    name: 'home',       component: HomeComponent,               meta: { title: 'Home'} },
+        { path: '/about',                   name: 'about',      component: AboutComponent,              meta: { title: 'About us'} },
+        { path: '/publishing/categories',   name: 'categories', component: CategoryManagementComponent, meta: { title: 'Categories'}},
+        { path: '/',                        redirect: '/home' }
     ]
 });
 

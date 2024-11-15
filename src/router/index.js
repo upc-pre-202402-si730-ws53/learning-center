ef4224732797c698bf537ccc0630b3936665b1f7
Angel Velasquez <pcisavel@upc.edu.pe>
2024-11-15 17:16:59 -0500

    feat(iam): add sign-up and sign-in navigation routes.

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import {createRouter, createWebHistory} from "vue-router";
 import HomeComponent from "../public/pages/home.component.vue";
 import AboutComponent from "../public/pages/about.component.vue";
 import CategoryManagementComponent from "../publishing/pages/category-management.component.vue";
+import SignInComponent from "../iam/pages/sign-in.component.vue";
+import SignUpComponent from "../iam/pages/sign-up.component.vue";
 
 const router = createRouter({
     history: createWebHistory(),
@@ -9,6 +11,8 @@ const router = createRouter({
         { path: '/home',                    name: 'home',       component: HomeComponent,               meta: { title: 'Home'} },
         { path: '/about',                   name: 'about',      component: AboutComponent,              meta: { title: 'About us'} },
         { path: '/publishing/categories',   name: 'categories', component: CategoryManagementComponent, meta: { title: 'Categories'}},
+        { path: '/sign-in',                 name: 'sign-in',    component: SignInComponent,             meta: { title: 'Sign In'}},
+        { path: '/sign-up',                 name: 'sign-up',    component: SignUpComponent,             meta: { title: 'Sign Up'}},
         { path: '/',                        redirect: '/home' }
     ]
 });
